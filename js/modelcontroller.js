3dab39254355fc73afb6c8cb5a5b85215be1df4c
Robert Rangi <robert_rangi@xtra.co.nz>
2013-08-12 04:06:24 +1200

    Rapid 130812.0.1
    Corrections to the Responsive SCSS
    Clean up the assets

--- js/modelcontroller.js
+++ js/modelcontroller.js
@@ -6,13 +6,14 @@ var appRapid = angular.module('appRapid', ['ui.bootstrap.collapse']);
 // =========================================================================
 appRapid.config(function ($routeProvider) {
     $routeProvider
-        .when('/home', { controller: 'pitchController', templateUrl: 'htm/view-home.htm'})
-        .when('/about', { controller: 'simpleController', templateUrl: 'htm/view-about.htm'})
+        .when('/', { controller: 'cntlHome', templateUrl: 'htm/view-home.htm'})
+        .when('/services', { controller: 'cntlHome', templateUrl: 'htm/view-home.htm'})
+        .when('/about', { controller: 'aboutController', templateUrl: 'htm/view-about.htm'})
         .when('/courses', { controller: 'courseController', templateUrl: 'htm/view-courses.htm'})
         .when('/media', { controller: 'simpleController', templateUrl: 'htm/view-media.htm'})
         .when('/blog', { controller: 'simpleController', templateUrl: 'htm/view-blog.htm'})
         .when('/contact', { controller: 'simpleController', templateUrl: 'htm/view-contact.htm'})
-        .otherwise({ redirectTo: '/home' });
+        .otherwise({ redirectTo: '/' });
 });
 
 // The factories for the data - connect to external CMS, DB etc in future
@@ -21,7 +22,8 @@ appRapid.factory('rapidMenu', function() {
     var rapidMenu = {};
 
     rapidMenu.menuItems = 
-    [ {menuRoute: '/home', menuIcon: 'icon-home', menuName: 'Home'}
+    [ {menuRoute: '/', menuIcon: 'icon-home', menuName: 'Home'}
+    , {menuRoute: '/services', menuIcon: 'icon-cog', menuName: 'Services'}
     , {menuRoute: '/about', menuIcon: 'icon-cog', menuName: 'About'}
     , {menuRoute: '/courses', menuIcon: 'icon-user', menuName: 'Courses'}
     , {menuRoute: '/media', menuIcon: 'icon-camera', menuName: 'Media'}
@@ -44,11 +46,20 @@ controllers.menuController = function ($scope, $location, rapidMenu) {
 
 // Model Controller for the Sales-Pitch section
 // ============================================================
-controllers.pitchController = function($scope, $http) {
+controllers.cntlHome = function($scope, $http) {
     $http.get('txt/modelHome.json').success(function(data){
-        $scope.homeCntl = data;
+        $scope.dataHome = data;
     });
 }
+
+// Model Controller for the About view
+// ============================================================
+controllers.aboutController = function ($scope, $http) {
+    $http.get('txt/modelAbout.json').success(function(data){
+        $scope.aboutCntl = data;
+    });
+}
+
 
 // Model Controller for the Courses.htm
 // ============================================================
