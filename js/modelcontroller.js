ef3f53efe0a1c9c7372b6046702cd1bfb2ea52ee
Robert Rangi <robert_rangi@xtra.co.nz>
2013-08-14 07:11:04 +1200

    Rapid 130814.0.1

--- js/modelcontroller.js
+++ js/modelcontroller.js
@@ -4,16 +4,35 @@ var appRapid = angular.module('appRapid', ['ui.bootstrap.collapse']);
 
 // The router load the correct view and passed the corresponding Controller
 // =========================================================================
-appRapid.config(function ($routeProvider) {
+appRapid.config(function($routeProvider) {
     $routeProvider
-        .when('/', { controller: 'cntlHome', templateUrl: 'htm/view-home.htm'})
-        .when('/services', { controller: 'cntlHome', templateUrl: 'htm/view-home.htm'})
-        .when('/about', { controller: 'aboutController', templateUrl: 'htm/view-about.htm'})
-        .when('/courses', { controller: 'courseController', templateUrl: 'htm/view-courses.htm'})
-        .when('/media', { controller: 'simpleController', templateUrl: 'htm/view-media.htm'})
-        .when('/blog', { controller: 'simpleController', templateUrl: 'htm/view-blog.htm'})
-        .when('/contact', { controller: 'simpleController', templateUrl: 'htm/view-contact.htm'})
-        .otherwise({ redirectTo: '/' });
+        .when('/', {
+            controller: 'cntlHome',
+            templateUrl: 'htm/view-home.htm'
+        })
+        .when('/services', {
+            controller: 'cntlSimple',
+            templateUrl: 'htm/view-services.htm'
+        })
+        .when('/about', {
+            controller: 'cntlAbout',
+            templateUrl: 'htm/view-about.htm'
+        })
+        .when('/media', {
+            controller: 'cntlSimple',
+            templateUrl: 'htm/view-media.htm'
+        })
+        .when('/blog', {
+            controller: 'cntlSimple',
+            templateUrl: 'htm/view-blog.htm'
+        })
+        .when('/contact', {
+            controller: 'cntlSimple',
+            templateUrl: 'htm/view-contact.htm'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
 });
 
 // The factories for the data - connect to external CMS, DB etc in future
@@ -21,14 +40,31 @@ appRapid.config(function ($routeProvider) {
 appRapid.factory('rapidMenu', function() {
     var rapidMenu = {};
 
-    rapidMenu.menuItems = 
-    [ {menuRoute: '/', menuIcon: 'icon-home', menuName: 'Home'}
-    , {menuRoute: '/services', menuIcon: 'icon-cog', menuName: 'Services'}
-    , {menuRoute: '/about', menuIcon: 'icon-cog', menuName: 'About'}
-    , {menuRoute: '/courses', menuIcon: 'icon-user', menuName: 'Courses'}
-    , {menuRoute: '/media', menuIcon: 'icon-camera', menuName: 'Media'}
-    , {menuRoute: '/blog', menuIcon: 'icon-book', menuName: 'Blog'}
-    , {menuRoute: '/contact', menuIcon: 'icon-envelope', menuName: 'Contact'}];
+    rapidMenu.menuItems = [{
+        menuRoute: '/',
+        menuIcon: 'icon-home',
+        menuName: 'Home'
+    }, {
+        menuRoute: '/services',
+        menuIcon: 'icon-cog',
+        menuName: 'Services'
+    }, {
+        menuRoute: '/about',
+        menuIcon: 'icon-cog',
+        menuName: 'About'
+    }, {
+        menuRoute: '/media',
+        menuIcon: 'icon-camera',
+        menuName: 'Media'
+    }, {
+        menuRoute: '/blog',
+        menuIcon: 'icon-book',
+        menuName: 'Blog'
+    }, {
+        menuRoute: '/contact',
+        menuIcon: 'icon-envelope',
+        menuName: 'Contact'
+    }];
 
     return rapidMenu;
 })
@@ -39,37 +75,43 @@ var controllers = {};
 
 // Controls the Menu bar routenames, items and icons
 // ============================================================
-controllers.menuController = function ($scope, $location, rapidMenu) {
+controllers.menuController = function($scope, $location, rapidMenu) {
     $scope.location = $location;
-    $scope.menuItems = rapidMenu.menuItems;   
+    $scope.menuItems = rapidMenu.menuItems;
 };
 
 // Model Controller for the Sales-Pitch section
 // ============================================================
 controllers.cntlHome = function($scope, $http) {
-    $http.get('txt/modelHome.json').success(function(data){
+    $http.get('txt/modelHome.json').success(function(data) {
         $scope.dataHome = data;
     });
 }
 
 // Model Controller for the About view
 // ============================================================
-controllers.aboutController = function ($scope, $http) {
-    $http.get('txt/modelAbout.json').success(function(data){
-        $scope.aboutCntl = data;
+controllers.cntlAbout = function($scope, $http) {
+    $http.get('txt/modelAbout.json').success(function(data) {
+        $scope.dataAbout = data;
     });
 }
-
 
 // Model Controller for the Courses.htm
 // ============================================================
-controllers.courseController = function ($scope, $http) {
-    $http.get('txt/modelCourse.json').success(function(data){
-        $scope.courseCntl = data;
+controllers.cntlCourse = function($scope, $http) {
+    $http.get('txt/modelCourse.json').success(function(data) {
+        $scope.dataCourse = data;
     });
 }
 
-controllers.simpleController = function ($scope) {
-};
+// Model Controller for the Blog view
+// ============================================================
+controllers.cntlBlog = function($scope, $http) {
+    $http.jsonp('//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=http://bjosman.wordpress.com/feed/').success(function(data) {
+        $scope.dataBlog = data.responseData.feed.entries;
+    });
+}
+
+controllers.cntlSimple = function($scope) {};
 
 appRapid.controller(controllers);
